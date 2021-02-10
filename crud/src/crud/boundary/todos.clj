2499b9fb5b73a278a425e120a42c8c7b26314ca3
ekusonarot <toranosuke707@gmail.com>
2021-02-10 20:20:10 +0900

    fin

--- crud/src/crud/boundary/todos.clj
+++ crud/src/crud/boundary/todos.clj
@@ -2,11 +2,19 @@
   (:require [clojure.java.jdbc :as jdbc]))
 
 (defprotocol Todos
-  (get-todos [db]))
+  (get-todos [db])
+  (create-todo [db params])
+  (delete-todo [db id]))
 
 (extend-protocol Todos
   duct.database.sql.Boundary
   
   (get-todos [{:keys [spec]}]
     (jdbc/query spec ["SELECT * FROM todos"]))
+
+  (create-todo [{:keys [spec]} params]
+    (jdbc/insert! spec :todos {:title (:title params)}))
+
+  (delete-todo [{:keys [spec]} id]
+    (jdbc/delete! spec :todos ["id=?" id]))
   )
--- crud/src/crud/handler/todos.clj
+++ crud/src/crud/handler/todos.clj
@@ -8,3 +8,14 @@
   (fn [{[_] :ataraxy/result}]
     (let [todos (todos/get-todos db)]
       [::response/ok todos])))
+
+(defmethod ig/init-key ::create [_ {:keys [db]}]
+  (fn [{[_ params] :ataraxy/result}]
+    (let [result (todos/create-todo db params)
+          id (:id (first result))]
+      [::response/created (str "/todos/" id)])))
+
+(defmethod ig/init-key ::delete [_ {:keys [db]}]
+  (fn [{[_ id] :ataraxy/result}]
+    (todos/delete-todo db id)
+    [::response/no-content]))
